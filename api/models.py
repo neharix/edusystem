3a3cd95838607cad2d4461751583dc63685c8d6c
neharix <142607660+neharix@users.noreply.github.com>
2024-12-20 23:01:06 +0500

    student models added

--- api/models.py
+++ api/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -71,3 +73,36 @@ class Specialization(models.Model):
 
     def __str__(self):
         return f"{self.year} {self.seed.name} {self.index}"
+
+
+class Student(models.Model):
+    class Gender(models.TextChoices):
+        MALE = "M", "Erkek"
+        FEMALE = "F", "Gyz"
+
+    class FamilyStatus(models.TextChoices):
+        pass
+
+    class PaymentType(models.TextChoices):
+        pass
+
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    patronymic = models.CharField(max_length=100)
+    gender = models.CharField(max_length=1, choices=Gender.choices)
+    family_status = models.CharField(max_length=3, choices=FamilyStatus.choices)
+    payment_type = models.CharField(max_length=2, choices=PaymentType.choices)
+    nationality = models.ForeignKey("Nationality", on_delete=models.PROTECT)
+    country = models.ForeignKey("Country", on_delete=models.PROTECT)
+    region = models.ForeignKey("Region", on_delete=models.PROTECT)
+    specialization = models.ForeignKey("Specialization", on_delete=models.PROTECT)
+    birth_date = models.DateField(default=datetime.date(1970, 1, 1))
+    admission_type = models.DateField()
+    registered_place = models.TextField()
+    phone_number = models.CharField(max_length=20)
+    passport = models.CharField(max_length=20)
+    military_service = models.CharField(max_length=20, blank=True, null=True)
+    label = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.surname} {self.name} {self.patronymic}"
