f6533a7ea2df900fd66d4252c6c20d62cd2a39ee
neharix <142607660+neharix@users.noreply.github.com>
2025-02-05 00:33:20 +0500

    update

---
+++ src/views/FacultyViews/FacultiesListView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import {useHighSchoolsStore} from "@/stores/api.store.js";
+import HighSchoolsDataTable from "@/components/DataTables/HighSchoolsDataTable.vue";
+import {storeToRefs} from "pinia";
+import TheBreadcrumb from "@/components/TheBreadcrumb.vue";
+import {onMounted, watch} from "vue";
+
+const highSchoolsStore = useHighSchoolsStore();
+const {highSchools} = storeToRefs(highSchoolsStore);
+
+onMounted(() => {
+  highSchoolsStore.getAllAdditional()
+})
+
+const breadcrumbPaths = [
+  {path: "/high-schools", name: "Ýokary okuw mekdepleri"},
+  {path: "/high-schools/add", name: "Goşmak"},
+]
+
+</script>
+
+<template>
+  <div class="w-full">
+    <the-breadcrumb :paths="breadcrumbPaths"></the-breadcrumb>
+    <high-schools-data-table :data="highSchools" @update="highSchoolsStore.getAllAdditional()"></high-schools-data-table>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
