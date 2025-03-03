fc19bdb0ed9282b594a0f9bd5da0766835a89eb4
neharix <142607660+neharix@users.noreply.github.com>
2025-03-03 09:44:55 +0500

    commit

---
+++ dist/assets/Page404-CmUOirr9.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./TheError-46O-1taw.js";import{c as e,b as s}from"./index-CxU83Qru.js";const m={__name:"Page404",setup(t){return(o,r)=>(s(),e(a,{"status-code":404,message:"sahypa tapylmady","is-home-link-enabled":!0}))}};export{m as default};
