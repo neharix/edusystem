d28b623725c9d16a7974f99c18102d6367fcde5c
neharix <azatjurjenov24@gmail.com>
2025-06-14 18:04:01 +0500

    update

--- dist/assets/Classificators-CB0txRN4.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,c as r,b as c,a as t}from"./index-DtGnqybP.js";const s={};function n(a,_){const e=t("router-view");return c(),r(e)}const i=o(s,[["render",n]]);export{i as default};
--- dist/assets/SecondarySchoolsView-DK2uhx2y.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,f as c,b as o}from"./index-DtGnqybP.js";const n={};function r(s,t){return o(),c("p",null,"secondary-schools")}const _=e(n,[["render",r]]);export{_ as default};
